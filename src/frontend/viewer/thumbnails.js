5ca7ee00afc140eb33d5ef3c43e9e57d38332224
elsie <yubisiba@gmail.com>
2025-10-21 11:38:03 +0000

    viewerのrandomフェッチを修正

--- src/frontend/viewer/thumbnails.js
+++ src/frontend/viewer/thumbnails.js
@@ -76,7 +76,7 @@ export function displayThumbnailImages(container, images, currentId, clearContai
 }
 
 export function fetchRandomImage() {
-	const cnt = window.width > window.height ? 10 : 12;
+	const cnt = window.innerWidth > window.innerHeight ? 10 : 12;
 	authenticatedFetch('/req/img/rand/' + cnt, { method: 'GET' })
 		.then(response => response.json())
 		.then(data => {
